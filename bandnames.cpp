4fc926d4d932458b48d57d6cb9e6fe83f7dfbecc
Mason Heller <masonph@gmail.com>
2012-03-19 12:38:29 -0700

    converted from functions to class

--- bandnames.cpp
+++ bandnames.cpp
@@ -33,345 +33,374 @@
 #include <ctime>
 using namespace std;
 
-bool dump=false;	// dump internal lists to data files
-bool bstat=false;	// enable data files by default
-bool nstat=false;	// disable internal lists? default (false) enable them
 
-#define LIST1 "list1.dat"
-#define LIST2 "list2.dat"
+#define FILE1 "list1.dat"
+#define FILE2 "list2.dat"
 
 
-struct wordlist
+class wordlist
 {
-	vector<string> word;
-};
-
-int stat(string nothing, wordlist &st)
-{
-	if(nothing == LIST1) // list 1
+	private:
+	vector<string> list1;
+	vector<string> list2;
+		
+	
+	public:
+	wordlist()
 	{
-		st.word= {
-"Aardvark",
-"Apocalypse",
-"Bad",
-"Bikini",
-"Boston",
-"Breadstick",
-"Broken",
-"Bucket and",
-"Camden",
-"Captain",
-"Cephalopod",
-"Chekhovs",
-"Coastal",
-"Covers",
-"Death",
-"Death-Pit",
-"Delicious",
-"Delta",
-"Digital",
-"Donkey",
-"Dr.",
-"Eccentric",
-"Electric",
-"Estimated",
-"Faceless",
-"Facial",
-"Fallen",
-"Father",
-"Fever Dream",
-"Fiendish",
-"Five Alarm",
-"Foot",
-"Four Alarm",
-"Fretless",
-"Galileos",
-"Gin and",
-"Goony",
-"Grizzly Bear",
-"Grum",
-"Guest",
-"Hardlaser",
-"Harmonicus",
-"Hedgehog",
-"Hopopotamus",
-"Horse",
-"Hound",
-"Jefferson",
-"Kafkas",
-"Killa",
-"Killasaurus",
-"Languishing",
-"Loading",
-"Lord",
-"Lunchbox of",
-"Luv",
-"Mantiss",
-"Masterful",
-"Men With",
-"Metallic",
-"Metric",
-"Mikey and",
-"Miss",
-"Monkey",
-"Moustache",
-"Mr.",
-"Orange",
-"Original",
-"Orlando",
-"Penguin",
-"Penny",
-"Perspiring",
-"Pixelface",
-"Prague",
-"Raspberry",
-"Razorblade",
-"Real",
-"Salt Horse",
-"Sir",
-"Snuggle",
-"Stir-Fry",
-"Supercilious",
-"Taming",
-"Taxonomic",
-"Texas",
-"The",
-"The Babbling",
-"The Cabinet",
-"The Damp",
-"The Doo-Wah",
-"The Empty",
-"The Grunting",
-"The Holy",
-"The Howler",
-"The Hyperbolic",
-"The Inverted",
-"The Low",
-"The Navel",
-"The Nose",
-"The Odious",
-"The Shaven",
-"The Stylish",
-"The Totality",
-"The Whining",
-"Three Alarm",
-"Toasty",
-"Tog",
-"Try for",
-"Ultra",
-"Unicorn",
-"Vapor",
-"Waxy",
-"Weekend of",
-"Whiteboard",
-"Wooly",
-"Wynnum"};
+		list1 = {
+		"Aardvark",
+		"Apocalypse",
+		"Bad",
+		"Bikini",
+		"Boston",
+		"Breadstick",
+		"Broken",
+		"Bucket and",
+		"Camden",
+		"Captain",
+		"Cephalopod",
+		"Chekhovs",
+		"Coastal",
+		"Covers",
+		"Death",
+		"Death-Pit",
+		"Delicious",
+		"Delta",
+		"Digital",
+		"Donkey",
+		"Dr.",
+		"Eccentric",
+		"Electric",
+		"Estimated",
+		"Faceless",
+		"Facial",
+		"Fallen",
+		"Father",
+		"Fever Dream",
+		"Fiendish",
+		"Five Alarm",
+		"Foot",
+		"Four Alarm",
+		"Fretless",
+		"Galileos",
+		"Gin and",
+		"Goony",
+		"Grizzly Bear",
+		"Grum",
+		"Guest",
+		"Hardlaser",
+		"Harmonicus",
+		"Hedgehog",
+		"Hopopotamus",
+		"Horse",
+		"Hound",
+		"Jefferson",
+		"Kafkas",
+		"Killa",
+		"Killasaurus",
+		"Languishing",
+		"Loading",
+		"Lord",
+		"Lunchbox of",
+		"Luv",
+		"Mantiss",
+		"Masterful",
+		"Men With",
+		"Metallic",
+		"Metric",
+		"Mikey and",
+		"Miss",
+		"Monkey",
+		"Moustache",
+		"Mr.",
+		"Orange",
+		"Original",
+		"Orlando",
+		"Penguin",
+		"Penny",
+		"Perspiring",
+		"Pixelface",
+		"Prague",
+		"Raspberry",
+		"Razorblade",
+		"Real",
+		"Salt Horse",
+		"Sir",
+		"Snuggle",
+		"Stir-Fry",
+		"Supercilious",
+		"Taming",
+		"Taxonomic",
+		"Texas",
+		"The",
+		"The Babbling",
+		"The Cabinet",
+		"The Damp",
+		"The Doo-Wah",
+		"The Empty",
+		"The Grunting",
+		"The Holy",
+		"The Howler",
+		"The Hyperbolic",
+		"The Inverted",
+		"The Low",
+		"The Navel",
+		"The Nose",
+		"The Odious",
+		"The Shaven",
+		"The Stylish",
+		"The Totality",
+		"The Whining",
+		"Three Alarm",
+		"Toasty",
+		"Tog",
+		"Try for",
+		"Ultra",
+		"Unicorn",
+		"Vapor",
+		"Waxy",
+		"Weekend of",
+		"Whiteboard",
+		"Wooly",
+		"Wynnum"};
+		list2 = {
+		"Agony",
+		"Amplitude",
+		"and the Hex",
+		"Apples",
+		"Arcade",
+		"Atrophy",
+		"Babes",
+		"Baggage",
+		"Baptistes",
+		"Barbers",
+		"Bees",
+		"Blood",
+		"Bonnets",
+		"Brain Stem",
+		"Bubbles",
+		"Byron",
+		"Cannon",
+		"Captains",
+		"Cart",
+		"Charismatics",
+		"Chow",
+		"Circus",
+		"Coma",
+		"Commas",
+		"Contacts",
+		"Cordial",
+		"Culture",
+		"Cybersquad",
+		"Death",
+		"Diddies",
+		"Diet",
+		"Disaster",
+		"Dog Party",
+		"Dornholes",
+		"Dropbear",
+		"Ether",
+		"Flavors",
+		"Fraternity",
+		"FreQs",
+		"Friday",
+		"Friends",
+		"Frijoles",
+		"Furnace",
+		"Gerbil",
+		"Glory Days",
+		"Goblins",
+		"Goons",
+		"Gun",
+		"Harmonicus",
+		"Hat",
+		"Heroes",
+		"Hill",
+		"Hipopotamus",
+		"Horse",
+		"Howler",
+		"Icarus",
+		"in a Day",
+		"Index",
+		"Kohlers",
+		"Legends",
+		"Lemonade",
+		"Lincoln",
+		"Lock",
+		"Machine",
+		"Mantiss",
+		"Mechanotherapy",
+		"Mistress",
+		"Monday",
+		"Monkeys",
+		"Monotremes",
+		"Moustache",
+		"Not Included",
+		"Notre Dame",
+		"of Nouns",
+		"Oranges",
+		"Pallor",
+		"Pants",
+		"Party",
+		"Pastries",
+		"Phase",
+		"Philosophers",
+		"Pixelface",
+		"Prophet",
+		"Psychos",
+		"Pump",
+		"Punkaceratops",
+		"Puppy",
+		"Ramp",
+		"Razor",
+		"Robots",
+		"Rotunda",
+		"Sally",
+		"Saturday",
+		"Sham",
+		"Silverfish",
+		"Simpatico",
+		"Smiths",
+		"Soul",
+		"Spiral",
+		"Spring",
+		"Stabwounds",
+		"Strumbar",
+		"Sunday",
+		"Surprise",
+		"Survivors",
+		"Sushi",
+		"Syrup",
+		"Taxonomy",
+		"Teutonics",
+		"the Crustacean",
+		"the Loaf Dogs",
+		"Theatrics",
+		"Therapy",
+		"Thorns",
+		"Thursday",
+		"Tipper",
+		"Tongue",
+		"Towel",
+		"Train",
+		"Tuesday",
+		"Undulates",
+		"Variant",
+		"Vees",
+		"Vicars",
+		"Visage",
+		"Warbear",
+		"Wednesday",
+		"Weekends",
+		"Weelie",
+		"Whiplash",
+		"With Thugs",
+		"Yaks"};
 	}
-	else if(nothing == LIST2) // list 2
-	{
-		st.word={
-"Agony",
-"Amplitude",
-"and the Hex",
-"Apples",
-"Arcade",
-"Atrophy",
-"Babes",
-"Baggage",
-"Baptistes",
-"Barbers",
-"Bees",
-"Blood",
-"Bonnets",
-"Brain Stem",
-"Bubbles",
-"Byron",
-"Cannon",
-"Captains",
-"Cart",
-"Charismatics",
-"Chow",
-"Circus",
-"Coma",
-"Commas",
-"Contacts",
-"Cordial",
-"Culture",
-"Cybersquad",
-"Death",
-"Diddies",
-"Diet",
-"Disaster",
-"Dog Party",
-"Dornholes",
-"Dropbear",
-"Ether",
-"Flavors",
-"Fraternity",
-"FreQs",
-"Friday",
-"Friends",
-"Frijoles",
-"Furnace",
-"Gerbil",
-"Glory Days",
-"Goblins",
-"Goons",
-"Gun",
-"Harmonicus",
-"Hat",
-"Heroes",
-"Hill",
-"Hipopotamus",
-"Horse",
-"Howler",
-"Icarus",
-"in a Day",
-"Index",
-"Kohlers",
-"Legends",
-"Lemonade",
-"Lincoln",
-"Lock",
-"Machine",
-"Mantiss",
-"Mechanotherapy",
-"Mistress",
-"Monday",
-"Monkeys",
-"Monotremes",
-"Moustache",
-"Not Included",
-"Notre Dame",
-"of Nouns",
-"Oranges",
-"Pallor",
-"Pants",
-"Party",
-"Pastries",
-"Phase",
-"Philosophers",
-"Pixelface",
-"Prophet",
-"Psychos",
-"Pump",
-"Punkaceratops",
-"Puppy",
-"Ramp",
-"Razor",
-"Robots",
-"Rotunda",
-"Sally",
-"Saturday",
-"Sham",
-"Silverfish",
-"Simpatico",
-"Smiths",
-"Soul",
-"Spiral",
-"Spring",
-"Stabwounds",
-"Strumbar",
-"Sunday",
-"Surprise",
-"Survivors",
-"Sushi",
-"Syrup",
-"Taxonomy",
-"Teutonics",
-"the Crustacean",
-"the Loaf Dogs",
-"Theatrics",
-"Therapy",
-"Thorns",
-"Thursday",
-"Tipper",
-"Tongue",
-"Towel",
-"Train",
-"Tuesday",
-"Undulates",
-"Variant",
-"Vees",
-"Vicars",
-"Visage",
-"Warbear",
-"Wednesday",
-"Weekends",
-"Weelie",
-"Whiplash",
-"With Thugs",
-"Yaks"};
-	}
+	~wordlist(){}
 
-	else
-	{
-		cerr << "Error: '" << nothing << "' is not a valid internal list." << endl;
-		return 1;
+	int load(bool verbose=false, bool nstat=false)
+	{	
+		int val = 0;
+		val = load(FILE1 ,list1, verbose, nstat);
+		val = val | load(FILE2, list2, verbose, nstat);
+		return val;
 	}
-
-	if(dump)
+	
+	int load(string filename, vector<string> &list, bool verbose=false, bool nstat=false)
+	{
+		int val = 0;
+		ifstream datfile;
+		datfile.open(filename.c_str());
+		if(!datfile.is_open())
+		{
+			if(verbose || nstat)
+				cerr << "Error: " << filename << " could not be opened for read.";
+			if(!nstat)
+			{
+				if(verbose)
+					cerr << " Using built-in list..." << endl;
+				val = 1;
+			}
+			else
+			{
+				cerr << " Try using '-s'." << endl;
+				return 2;
+			}
+		}
+		string templine;
+		while (!datfile.eof())
+		{
+			getline(datfile,templine);
+			// allow comments in the data files
+			if(templine[0]!='/' && templine[0]!='\n' && templine[0]!='#' && templine[0]!=';')
+			{
+				list.push_back(templine);
+			}
+		}
+		datfile.close();
+		return val;
+	}
+	
+	bool dump()
+	{
+		bool val = true;
+		val = dump(list1, FILE1) && dump(list2, FILE2);
+	}
+	bool dump(vector<string> &list, string filename)
 	{
 		ofstream outf;
-		outf.open(nothing.c_str());
+		outf.open(filename);
 		if (outf.is_open())
 		{
-			for(int i=0;i<st.word.size();i++)
+			for(int i=0;i<list.size();i++)
 			{
-				outf << st.word[i] << '\n';
+				outf << list[i] << '\n';
 			}
 			outf.close();
 		}
 		else{
 			outf.close();
-			cerr << "Error: Could not open file '" << nothing << "' for writing" << endl;
-			return 1;
+			cerr << "Error: Could not open file '" << filename << "' for writing" << endl;
+			return false;
 		}
+		return true;
 	}
-
-	return 0;
-}
-
-int structurise(string filename, wordlist &st)
-{
-	if(bstat)
-		return stat(filename,st);
-
-	ifstream datfile;
-	string templine;
-
-	// start reading datafiles into variables
-	datfile.open(filename.c_str());
-	if(!datfile.is_open())
+	
+	string random()
 	{
-		cerr << "Error: " << filename << " could not be opened for read.";
-		if(!nstat){
-			cerr << " Using built-in list..." << endl;
-			return stat(filename,st);
-		}
-		else{
-			cerr << " Try using '-s'." << endl;
-			return 2;
-		}
+		return random(1) + random(2);
 	}
-	while (!datfile.eof())
+	string random(int list)
 	{
-		getline(datfile,templine);
-		// allow comments in the data files
-		if(templine[0]!='/' && templine[0]!='\n' && templine[0]!='#' && templine[0]!=';')
+		srand(time(NULL));
+		int pos;
+		if (list==1)
 		{
-			st.word.push_back(templine);
+			pos = random() % list1.size();
+			return list1[pos];
 		}
+		if (list==2)
+		{
+			pos = random() % list2.size();
+			return list2[pos];
+		}
+		cerr << "Error: list " << list << " does not exist!";
+		return "";
 	}
-	datfile.close();
+	
+};
 
-	return 0;
-}
 
 int main (int argc, char *argv[])
 {
-	wordlist one;
-	wordlist two;
+	bool dump=false;	// dump internal lists to data files
+	bool bstat=false;	// enable data files by default
+	bool nstat=false;	// disable internal lists? default (false) enable them
+	bool verbose=false;	// don't output non-fatal errors
+
+	wordlist word;
 
 #ifdef WIN32
 	bool pauseAtEnd = true;	//most Windows users don't run from terminal, so pause at end.
@@ -385,8 +414,9 @@ int main (int argc, char *argv[])
 	 *  reasonable situations. Maybe I'll fix it, or maybe I'll just make my
 	 *  parsing functions better in the future.
 	 */
-	if(argc>1){
-		if(strcmp(argv[1] , "-h")==0 || strcmp(argv[1] , "--help")==0 || strcmp(argv[1] , "/?")==0)
+	for(int i=1; i < argc; i++)
+	{
+		if(strcmp(argv[i] , "-h")==0 || strcmp(argv[i] , "--help")==0 || strcmp(argv[i] , "/?")==0)
 		{
 			cout 
 			<< "Rock Band name generator\n"
@@ -406,6 +436,7 @@ int main (int argc, char *argv[])
 			#else
 			<< "  -p               pause after display\n"
 			#endif
+			<< "  -v               display non-fatal errors\n"
 			<< "      --dump       dump built-in lists to data files\n"
 			<< "  -h, --help, /?   display this help and exit\n"
 			<< "\nThe words for these lists were found on the internet somewhere.\n"
@@ -414,51 +445,46 @@ int main (int argc, char *argv[])
 			<< endl;
 			return 0;
 		}
-		else if(strcmp(argv[1] , "--dump")==0)
+		else if(strcmp(argv[i] , "--dump")==0)
 		{
-			dump=true;
-			stat(LIST1,one);
-			stat(LIST2,two);
+			word.dump();
 			return 0;
 		}
-		else if(strcmp(argv[1] , "--static")==0 || strcmp(argv[1] , "-s")==0)
+		else if(strcmp(argv[i] , "--static")==0 || strcmp(argv[i] , "-s")==0)
 		{
 			bstat=true;
 		}
-		else if(strcmp(argv[1] , "--nstatic")==0 || strcmp(argv[1] , "-n")==0)
+		else if(strcmp(argv[i] , "--nstatic")==0 || strcmp(argv[i] , "-n")==0)
 		{
 			bstat=false;
 		}
-		else if(strcmp(argv[1] , "--nostatic")==0 || strcmp(argv[1] , "-m")==0)
+		else if(strcmp(argv[i] , "--nostatic")==0 || strcmp(argv[i] , "-m")==0)
 		{
 			nstat=true;
 			bstat=false;
 		}
-		else if(strcmp(argv[1],"-p")==0)
+		else if(strcmp(argv[i],"-p")==0)
 			pauseAtEnd = true;
-		else if(strcmp(argv[1],"-np")==0)
+		else if(strcmp(argv[i],"-np")==0)
 			pauseAtEnd = false;
+		else if(strcmp(argv[i],"-v")==0)
+			verbose=true;
 		else{
-			cout << "Unrecognised argument: '" << argv[1] << "'\n";
+			cout << "Unrecognised argument: '" << argv[i] << "'\n";
 			cout << "Try 'bofh -h' for a list of valid arguments" << endl;
 			return 0;
 		}
 	}
 
-	if(structurise(LIST1,one)==2){
-		cerr << "Fatal Error. Exiting..." << endl;
-		return 1;
-	}
-	if(structurise(LIST2,two)==2){
-		cerr << "Fatal Error. Exiting..." << endl;
-		return 1;
-	}
+	if(!bstat)
+		if(word.load()>1){
+			cerr << "Fatal Error. Exiting..." << endl;
+			return 1;
+		}
 
 
-	srand(time(NULL));
 
-	cout << one.word[rand()%one.word.size()]
-		<< " " << two.word[rand()%two.word.size()] << endl;
+	cout << word.random() << endl;
 	
 
 	if(pauseAtEnd)
